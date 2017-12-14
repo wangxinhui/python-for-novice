0d45617fbe4f6d02de6eac5c635289676fb4e748
wangxinhui <wangxinhuileft@163.com>
2017-12-14 23:23:04 +0800

    第二章结束

--- lesson-2/lesson-2-1.py
+++ lesson-2/lesson-2-1.py
@@ -6,3 +6,32 @@ print(message)
 
 message = 'Hello Python Crash Course world!'
 print(message)
+
+# 字符串
+# 用引号括起的都是字符串,其中的引号可以是单引号,也可以是双引号
+"this is a String"
+'this is  also a String'
+'I told my friend, "Python is my favorite language!"'
+"The language 'Python' is named after Monty Python, not the snake."
+"One of Python's strengths is its diverse and supportive community."
+
+# 使用方法修改字符串的大小写(首字母大写)
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+# 合并(拼接)字符串
+first_name = "xinhui"
+last_name = "wang"
+full_name = first_name + " " + last_name
+print(full_name)
+
+# 使用制表符或换行符来添加空白
+print("\tPython")
+print("\n\tPython")
+# 删除空白 rstrip(右) lstrip（左） strip（两边）
+favorite_language = 'python '
+print(favorite_language.rstrip())
+print(favorite_language)
+
---
+++ lesson-2/lesson-2-2.py
@@ -0,0 +1,15 @@
+# 数字 + - * /  **(平方)
+print(2+3)
+print(1+7)
+print(16/2)
+print(9-1)
+print(2**3)
+print(2*4)
+#  Python 2 中的整数 是1 3.0 /2
+print(3/2)
+# 浮点型
+print(0.2 + 0.1) #结果是0.30000000000000004 暂时忽略多余的小数位数即可;后面的学习在需要时处理多余小数位的方式。
+#使用函数str()避免类型错误
+age =23
+message = "Happy " + str(age) + "rd Birthday"
+print(message)
