3c913b626b98c776284cca16412e124ea3909324
wangxinhui <wangxinhuileft@163.com>
2017-12-14 18:40:17 +0800

    python 初学者

---
+++ lesson-2/lesson-2-1.py
@@ -0,0 +1,8 @@
+# 变量的命名和使用
+# 变量名只能包含字母、数字和下划线。变量名可以字母或下划线打头，但不能以数字打头，例如，可将变量命名为message_1，但不能将其命名为1_message。
+
+message = 'Hello python world!'
+print(message)
+
+message = 'Hello Python Crash Course world!'
+print(mesage)
