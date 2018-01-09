56d2a19ac4c8d569bc3718e3a4125cfef3341301
wangxinhui <wangxinhuileft@163.com>
2018-01-09 19:53:13 +0800

    第 + 章 继续...

---
+++ lesson10/lesson10-1.py
@@ -0,0 +1,7 @@
+# with open('pi_digits.txt') as file_object:
+#     contents = file_object.read()
+#     print(contents.rstrip())
+
+with open('pi_digits.txt') as file_object:
+    for line in file_object:
+        print(line.rstrip())
