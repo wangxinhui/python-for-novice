756c786360ac867640a45b3d75e13e70d0d46a4a
wangxinhui <wangxinhuileft@163.com>
2018-01-10 22:45:46 +0800

    will learn chapter 11

---
+++ lesson10/lesson10-4.py
@@ -0,0 +1,16 @@
+# 存储数据
+# json.load 和 json.dump
+import json
+number="123"
+number1="123"
+l = ['iplaypython',[1,2,3], {'name':'xiaoming'}]
+filename= 'json/numbers.json'
+with open(filename,'r+') as f_obj:
+    enjson = json.dumps(l)
+    dejson = json.loads(enjson)
+    print(type(enjson))
+
+
+# with open(filename,"r") as f_obj1:
+#     num = json.load(f_obj1)
+#     print(num)
