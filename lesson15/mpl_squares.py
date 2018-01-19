182e2664fc5a71b4698434ea2d72226561e39262
wangxinhui <wangxinhuileft@163.com>
2018-01-19 17:36:04 +0800

    chapter 15

---
+++ lesson15/mpl_squares.py
@@ -0,0 +1,13 @@
+#-*- coding：gbk -*-
+# matplotlib画廊
+# 绘制简单的折线图
+import matplotlib.pyplot as plt
+input_value=[1,2,3,4,5]
+squares = [1,4,9,16,25]
+plt.plot(input_value,squares,linewidth=5)
+# 修改标签文字和线条粗细
+plt.title('the squares')
+plt.xlabel("Value",fontsize=14)
+plt.ylabel("Square of Value",fontsize=14)
+plt.tick_params(axis='both',labelsize=14)
+plt.show()
---
+++ lesson15/scatter_squares.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+plt.scatter(2,4,s=100)
+plt.tick_params(axis='both',which='major',labelsize=14)
+plt.show()
